449b55190a3a2ccf7047393e7a84f3d9721cb182
Muhammad Hasyim Muladi <80388567+hasyim21@users.noreply.github.com>
2024-01-03 15:21:20 +0700

    update title app

--- lib/app.dart
+++ lib/app.dart
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Ecommerce',
+      title: 'SNKRS',
       theme: MyTheme.theme,
       home: const MainNavigationScreen(),
     );
